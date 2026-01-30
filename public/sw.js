9efffb1120df634d898da9ee6ec947a6bb307302
sazid-zero <sharif.sazid.3@gmail.com>
2026-01-30 17:40:51 +0600

    feat: add public/sw.js

---
+++ public/sw.js
@@ -0,0 +1,21 @@
+// Service Worker for PWA functionality
+const CACHE_NAME = 'linknest-v1'
+const urlsToCache = [
+  '/',
+  '/offline.html'
+]
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  )
+})
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+      .catch(() => caches.match('/offline.html'))
+  )
+})
